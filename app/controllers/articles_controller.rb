333104ea8658d56792d24cf32044b1e3ec184167
arturoli01 <arturo230801@gmail.com>
2019-07-11 19:46:54 -0500

    small fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   end
 
   def articles_params
-    params.require(:article).permit(:title, :description, :user_id)
+    params.require(:article).permit(:title, :description)
   end
 
 end
