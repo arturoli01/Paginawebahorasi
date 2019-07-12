0b2898010c175a0e289c6cd7dbefe635b8d70a73
arturoli01 <arturo230801@gmail.com>
2019-07-11 19:35:44 -0500

    Articles email auto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, except: [:new, :edit, :create, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   root :to => "home#home"
